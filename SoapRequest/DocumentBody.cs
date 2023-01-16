f68294132ac27021fd4cad3e2803907ce0b4a023
Recnir <danzasus@yandex.ru>
2023-01-16 15:02:07 +0400

    Изменение отправки запроса

--- SoapRequest/DocumentBody.cs
+++ SoapRequest/DocumentBody.cs
@@ -1,35 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Xml.Serialization;
 
 namespace SoapRequest
 {
-    public class DocumentBody
+  [Serializable]
+  [XmlRoot(ElementName = "eDocumentBody")]
+  public class DocumentBody : SAPRequestBody
+  {
+    [XmlIgnore]
+    private static DocumentBody instance;
+    new public static DocumentBody Instance
     {
-        private static DocumentBody instance;
-
-        public static DocumentBody Instance
-        {
-            get { return instance ?? (instance = new DocumentBody()); }
-        }
-
-        public string ID;
-
-        public string GUID;
-
-        public string Format;
-
-        public string RegistrationNumber;
-
-        public string CounterpartyName;
-
-        public string AlternativePayerOrPayee;
-
-        public string TotalAmount;
-
-        public string RelevanceSign;
-
-        public string Body;
-
+      get { return instance ?? (instance = new DocumentBody()); }
     }
+    public DocumentBody()
+    {
+      this.Namespace = "edoc";
+      this.NamespaceURI = "http://inform.gazprom.ru/C/IUSPT2/eDocumentBody";
+      this.NodeName = "eDocumentBody";
+    }
+
+    public string ID;
+
+    public string GUID;
+
+    public string Format;
+
+    public string RegistrationNumber;
+
+    public string CounterpartyName;
+
+    public string AlternativePayerOrPayee;
+
+    public string TotalAmount;
+
+    public string RelevanceSign;
+
+    public string Body;
+
+  }
 }
--- SoapRequest/DocumentTender.cs
+++ SoapRequest/DocumentTender.cs
@@ -1,36 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Xml.Serialization;
 
 namespace SoapRequest
 {
-    public class DocumentTender
+  [Serializable]
+  [XmlRoot("eDocumentTender")]
+  public class DocumentTender : SAPRequestBody
+  {
+    [XmlIgnore]
+    private static DocumentTender instance;
+    public static DocumentTender Instance
     {
-        private static DocumentTender instance;
-
-        public static DocumentTender Instance
-        {
-            get { return instance ?? (instance = new DocumentTender()); }
-        }
-
-        public string FinanceSource;
-
-        public string IDSAP;
-
-        public string OfferNumber;
-
-        public string DocumentBaseCode;
-
-        public string PurchaseType;
-
-        public string KindPursh;
-
-        public string KindTender;
-
-        public string FinanceArticle;
-
-        public string PurchaseMethod;
-
-        public string ExceptionFromSMSP;
+      get { return instance ?? (instance = new DocumentTender()); }
     }
+
+    public DocumentTender()
+    {
+      this.Namespace = "edoc";
+      this.NamespaceURI = "http://inform.gazprom.ru/C/IUSPT2/eDocumentTender";
+      this.NodeName = "eDocumentTender";
+    }
+
+    public string FinanceSource;
+
+    public string IDSAP;
+
+    public string OfferNumber;
+
+    public string DocumentBaseCode;
+
+    public string PurchaseType;
+
+    public string KindPursh;
+
+    public string KindTender;
+
+    public string FinanceArticle;
+
+    public string PurchaseMethod;
+
+    public string ExceptionFromSMSP;
+
+    public override string SerializeBody()
+    {
+      return base.SerializeBody(this.GetType(), this);
+    }
+  }
 }
